60bc0d3fa3b8edec632c0e283f8bbbc77c137bce
Farhad Gh <farhad.dev.contact@gmail.com>
2025-04-03 22:27:54 +0330

    Fix: input/output dir

--- scripts/config/settings.py
+++ scripts/config/settings.py
@@ -4,8 +4,8 @@
 AUTO_SELECT_FIRST_ITEM = True
 
 # File paths
-DEFAULT_INPUT_CSV_PATH = "data/input.csv"
-DEFAULT_OUTPUT_DIRECTORY = "data/output"
+DEFAULT_INPUT_CSV_PATH = "scripts/data/input.csv"
+DEFAULT_OUTPUT_DIRECTORY = "scripts/data/output"
 
 # Search settings
 BASE_SEARCH_URL = "https://mto.to/search?word="
